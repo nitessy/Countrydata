0997a6ad50bc4ec7d6359c77808597a8d3e20513
Nitesh Kumar Yadav <niteshyadav2107@gmail.com>
2023-06-27 20:12:16 +0530

    Add files via upload

---
+++ js/country.js
@@ -0,0 +1,71 @@
+const countryName = new URLSearchParams(location.search).get('name')
+const  flagImage = document.querySelector('.countrydetails img')
+const countryNameh1 = document.querySelector('.countrydetails h1')
+const nativeName = document.querySelector('.nativename')
+const population = document.querySelector('.population')
+const region = document.querySelector('.region')
+const subregion = document.querySelector('.subregion')
+const capital = document.querySelector('.capital')
+const toplevel = document.querySelector('.toplevel')
+const currencies = document.querySelector('.currency')
+const languages = document.querySelector('.languages')
+
+fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText = true`)
+.then((res)  =>  res.json())
+.then(([country]) => {
+    flagImage.src = country.flags.svg
+    countryNameh1.innerText = country.name.common
+
+    if(country.languages){
+        languages.innerText = Object.values(country.languages)[0]
+    }
+    
+
+    if (country.name.nativeName){
+        nativeName.innerText = Object.values(country.name.nativeName)[0].common
+    }
+    else{
+        nativeName.innerText = country.name.common
+
+    }
+    population.innerText = `${country.population.toLocaleString('en-IN')}`
+    region.innerText = country.region
+    subregion.innerText = country.subregion
+    capital.innerText = country.capital
+    toplevel.innerText = country.tld.join(' , ')
+
+    if(country.currencies){
+         currencies.innerText = Object.values(country.currencies).map((currency) => currency.name)[0]  }
+
+
+})
+
+
+
+
+
+// navbar 
+var fix = document.querySelector(".me")
+const change = document.querySelector(".navbar")
+const body = document.querySelector("body")
+const mobile_nav = document.querySelector(".button");
+const nav_header = document.querySelector(".header");
+const foot = document.querySelector(".footer")
+
+const togglenavbar = () =>{
+    nav_header.classList.toggle("active") ;
+    // if(fix.style.position === 'relative'){
+    //     fix.style.position === 'fixed' ; 
+    // } else{
+    //     fix.style.position=== 'relative'
+    // } 
+    fix.style.overflow == "hidden" ;
+     
+    // html.style.position = "fixed";
+  
+};
+
+mobile_nav.addEventListener("click", ()=> togglenavbar());
+
+
+
---
+++ js/script.js
@@ -0,0 +1,98 @@
+const countriescontainer = document.querySelector('.countries-container')
+// const search = document.querySelector('.search')
+// const countryName = document.querySelectorAll('.naam')
+const search = document.querySelector('.form input')
+var display
+var allCountriesData 
+fetch('https://restcountries.com/v3.1/subregion/Northern Africa')
+ .then((res) => res.json() )
+ .then((data) => {
+    renderCountries(data)
+     allCountriesData = data
+    
+ })
+ 
+    
+  
+
+
+
+  function renderCountries(data){
+    
+
+    data.forEach((country) => {
+       
+        const countryCard = document.createElement('a')
+        countryCard.classList.add('country')
+        countryCard.href = `country.html?name=${country.name.common}`
+        
+        
+        countryCard.innerHTML =  `                <img src="${country.flags.svg}">
+        <div class="card">
+        <h3 class="naam"> ${country.name.common}</h3>
+        <p> <b> Population: </b> ${country.population.toLocaleString('en-IN')}</p>
+        <p> <b> Region: </b>${country.region}</p>
+        <p> <b> Capital: </b>${country.capital}</p>  </div>`      
+        
+        
+        countriescontainer.append(countryCard)
+            }) 
+                
+             }   
+                
+
+          
+
+
+             fetch('https://restcountries.com/v3.1/all')
+             .then((res) => res.json() )
+             .then((data) => {
+                // renderCountries(data) 
+                
+              display = data} ) 
+               
+
+              
+                search.addEventListener('input' , (e) =>{
+                
+                 
+                 
+            
+            const filteredCountries = display.filter((country) => country.name.common.toLowerCase().includes(e.target.value.toLowerCase()))
+              console.log(filteredCountries) ;
+              countriescontainer.innerHTML = ''
+              renderCountries(filteredCountries)
+
+ })
+
+  
+ 
+
+
+ var fix = document.querySelector(".me")
+ const change = document.querySelector(".navbar")
+ const body = document.querySelector("body")
+ const mobile_nav = document.querySelector(".button");
+ const nav_header = document.querySelector(".header");
+
+
+
+
+
+
+ const togglenavbar = () =>{
+  nav_header.classList.toggle("active") ;
+  // if(fix.style.position === 'relative'){
+  //     fix.style.position === 'fixed' ; 
+  // } else{
+  //     fix.style.position=== 'relative'
+  // } 
+  // fix.style.overflow == "hidden" ;
+   
+  // html.style.position = "fixed";
+
+};
+
+mobile_nav.addEventListener("click", ()=> togglenavbar());
+
+
