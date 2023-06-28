0b64fa9bfd6c06a7507b59a9131dcd5c4a8bd6aa
Nitesh  Yadav <niteshyadav2107@gmail.com>
2023-06-28 15:51:05 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,24 @@
+var fix = document.querySelector(".me")
+const change = document.querySelector(".navbar")
+const body = document.querySelector("body")
+const mobile_nav = document.querySelector(".button");
+const nav_header = document.querySelector(".header");
+const foot = document.querySelector(".footer")
+
+const togglenavbar = () =>{
+    nav_header.classList.toggle("active") ;
+    // if(fix.style.position === 'relative'){
+    //     fix.style.position === 'fixed' ; 
+    // } else{
+    //     fix.style.position=== 'relative'
+    // } 
+    fix.style.overflow == "hidden" ;
+     
+    // html.style.position = "fixed";
+  
+};
+
+mobile_nav.addEventListener("click", ()=> togglenavbar());
+
+
+
